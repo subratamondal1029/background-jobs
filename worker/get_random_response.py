09060195331bb1007cd71fbb706f1a142c8d2b4d
Subrata Mondal <subratamondal@tutanota.com>
2026-02-22 19:54:32 +0530

    refactor: update API endpoint in get_random_response function and improve error handling

--- worker/get_random_response.py
+++ worker/get_random_response.py
@@ -2,14 +2,13 @@ import requests
 
 def get_random_response():
     try:
-        response = requests.get("http://localhost:5000/get-random")
+        response = requests.get("http://localhost:5000/get-random/s")
         result = response.json()
         if "error" in result:
             raise ValueError(result['error'])
         return result
-    except requests.RequestException as e:
-        print(f"Error fetching random user: {e}")
-        return {"error": str(e)}
+    except (requests.RequestException, ValueError):
+        raise
     
 
 print(get_random_response())
