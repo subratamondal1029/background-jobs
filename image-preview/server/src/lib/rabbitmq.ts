444cc03a1a14bcb4180df2f5fd7c5266326415a8
Subrata Mondal <subratamondal@tutanota.com>
2026-02-25 00:07:34 +0530

    feat: implement RabbitMQ connection and queue management

---
+++ image-preview/server/src/lib/rabbitmq.ts
@@ -0,0 +1,62 @@
+import amqplib from "amqplib";
+import requiredEnv from "@/utils/requiredEnv.js";
+
+const RABBITMQ_URL = requiredEnv("RABBITMQ_URL");
+const EXCHANGE_NAME = requiredEnv("EXCHANGE_NAME");
+const IMAGE_PROCESS_QUEUE = requiredEnv("IMAGE_PROCESS_QUEUE");
+
+let channel: amqplib.ConfirmChannel;
+
+const createQueue = async (queueName: string) => {
+  try {
+    await channel.assertQueue(queueName, {
+      durable: true,
+    });
+  } catch (error) {
+    console.error("Error creating queue:", error);
+    throw error;
+  }
+};
+
+const publishToQueue = (queueName: string, message: Record<string, any>) => {
+  if (!channel) {
+    throw new Error("RabbitMQ channel is not initialized");
+  }
+
+  const ok = channel.publish(
+    EXCHANGE_NAME,
+    queueName,
+    Buffer.from(JSON.stringify(message)),
+  );
+
+  if (!ok) {
+    console.warn("Backpressure: TCP buffer full")
+  }
+};
+
+const connectMQ = async () => {
+  try {
+    const conn = await amqplib.connect(RABBITMQ_URL);
+    channel = await conn.createConfirmChannel();
+    await channel.assertExchange(EXCHANGE_NAME, "direct", {
+      durable: true,
+    });
+
+    await createQueue(IMAGE_PROCESS_QUEUE);
+  } catch (error) {
+    console.error("Error connecting to RabbitMQ:", error);
+    throw error;
+  }
+};
+
+const disconnectMQ = async () => {
+  try {
+    if (channel) {
+      await channel.close();
+    }
+  } catch (error) {
+    console.error("Error disconnecting from RabbitMQ:", error);
+  }
+};
+
+export { channel, publishToQueue, connectMQ, disconnectMQ };
