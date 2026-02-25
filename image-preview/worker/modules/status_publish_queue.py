0cbf531272c33b6c82ddb08fb158299fe37a8742
Subrata Mondal <subratamondal@tutanota.com>
2026-02-26 00:13:49 +0530

    feat: implement status publishing functionality with message structure and queue setup

---
+++ image-preview/worker/modules/status_publish_queue.py
@@ -0,0 +1,32 @@
+import pika
+from pika.adapters.blocking_connection import BlockingChannel
+from modules.getenv import getenv
+from typing import TypedDict
+
+class StatusMessage(TypedDict):
+    jobId: int
+    status: str
+    previewUrl: str | None
+    error: str | None
+
+STATUS_EXCHANGE = getenv("STATUS_EXCHANGE")
+STATUS_QUEUE = getenv("STATUS_QUEUE")
+
+
+def init(ch: BlockingChannel):
+    ch.exchange_declare(exchange=STATUS_EXCHANGE, exchange_type="direct", durable=True)
+    ch.queue_declare(
+        queue=STATUS_QUEUE,
+        durable=True,
+        
+    )
+
+    ch.queue_bind(queue=STATUS_QUEUE, exchange=STATUS_EXCHANGE, routing_key=STATUS_QUEUE)
+
+def publish_status(ch: BlockingChannel, message: StatusMessage):
+    ch.basic_publish(
+        exchange=STATUS_EXCHANGE,
+        routing_key=STATUS_QUEUE,
+        body=str(message),
+        properties=pika.BasicProperties(delivery_mode=2)
+    )
