dae6e21538b49cba1f5c26cb7c16cdb177bd519a
Subrata Mondal <subratamondal@tutanota.com>
2026-02-26 00:02:15 +0530

    feat: enhance image preview generation with improved error handling and job status updates

--- image-preview/worker/modules/preview_generator.py
+++ image-preview/worker/modules/preview_generator.py
@@ -1,8 +1,10 @@
 import time
-import random
 import json
 from decimal import Decimal
 from typing import TypedDict
+
+from PIL import Image
+from io import BytesIO
 
 from pika import spec
 from pika.adapters.blocking_connection import BlockingChannel
@@ -10,13 +12,33 @@ from pika.adapters.blocking_connection import BlockingChannel
 from modules.getenv import getenv
 from modules.retry_queue_producer import publish_retry
 
+from modules.database_service import DatabaseService
+from modules.storage_service import Storage_service
+
+database_service = DatabaseService()
+storage_service = Storage_service()
+
 MAX_RETRIES = int(getenv("MAX_RETRIES"))
 
 
 class Data(TypedDict):
-    size: int
+    size: str
     imageId: str
     jobId: int
+
+def create_image(image_id: str, size: str):
+    formatted_size = size.split("x")
+    original_image_key = database_service.get_image_key(image_id)
+    original_image_data = storage_service.read_image(original_image_key)
+
+    img = Image.open(BytesIO(original_image_data))
+    img.thumbnail((int(formatted_size[0]), int(formatted_size[1])))
+
+    buff = BytesIO()
+    img.save(buff, format="JPEG", quality=85)
+    buff.seek(0)
+
+    return buff
 
 
 def generate_preview(
@@ -28,8 +50,15 @@ def generate_preview(
     try:
         headers = properties.headers or {}
         data: Data = json.loads(body)
+        database_service.update_job_status(data["jobId"], "PROCESSING")
         time.sleep(5)
-        print(data)
+        image_id = data["imageId"]
+        image_data = create_image(image_id, data["size"])
+        preview_key = storage_service.upload_image(image_data)
+
+        database_service.update_image_preview(image_id, preview_key)
+        database_service.update_job_status(data["jobId"], "SUCCESS")
+        # TODO: send to status queue
 
         channel.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
@@ -43,8 +72,17 @@ def generate_preview(
         if retry_count >= MAX_RETRIES:
             # add to update database
             print(f"Max retries exceeded for job {data['jobId']}")
+            database_service.update_job_status(
+                data["jobId"],
+                "FAILED",
+                error=f"Max retries exceeded for error: {str(e)}",
+                retries=retry_count,
+            )
             channel.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
             return
 
         publish_retry(channel, body, retry_count + 1)
+        database_service.update_job_status(
+            data["jobId"], "RETRYING", error=str(e), retries=retry_count + 1
+        )
         channel.basic_ack(delivery_tag=method.delivery_tag)
