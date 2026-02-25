92efa9e6afcad32f3c87f92a9814c249df3444c1
Subrata Mondal <subratamondal@tutanota.com>
2026-02-25 20:04:58 +0530

    feat: implement image controller with upload, retrieval, and deletion functionalities

---
+++ image-preview/server/src/controllers/image.controller.ts
@@ -0,0 +1,82 @@
+import asyncHandler from "@/utils/asyncHandler";
+import ApiError from "@/utils/ApiError";
+import ApiResponse from "@/utils/ApiResponse";
+import prisma from "@/lib/prisma";
+import type { Request, Response } from "express";
+import uploadService from "@/services/upload.service";
+
+const getImageEntry = async (id: string) => {
+  const image = await prisma.image.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!image) {
+    throw new ApiError(404, "Image not found");
+  }
+
+  return image;
+};
+
+const upload = asyncHandler(async (req: Request, res: Response) => {
+  const { key, filename, contentType } = await uploadService.uploadFile(req);
+
+  const image = await prisma.image.create({
+    data: {
+      originalKey: key,
+      filename,
+      contentType,
+    },
+  });
+
+  res
+    .status(201)
+    .json(
+      new ApiResponse({ id: image.id }, 201, "Image uploaded successfully"),
+    );
+});
+
+const getImage = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params as { id: string };
+
+  const image = await getImageEntry(id);
+
+  const stream = await uploadService.getFile(image.originalKey);
+
+  stream.pipe(res);
+});
+
+const getImagePreview = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params as { id: string };
+
+  const image = await getImageEntry(id);
+
+  if (!image.previewKey) {
+    throw new ApiError(404, "Preview not available");
+  }
+
+  const stream = await uploadService.getFile(image.previewKey);
+  stream.pipe(res);
+});
+
+const deleteImage = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params as { id: string };
+
+  const image = await getImageEntry(id);
+
+  await prisma.image.delete({
+    where: {
+      id,
+    },
+  });
+  
+  uploadService.deleteFile(image.originalKey);
+  if (image.previewKey) {
+    uploadService.deleteFile(image.previewKey);
+  }
+
+  res.sendStatus(204);
+});
+
+export { upload, getImage, getImagePreview, deleteImage };
