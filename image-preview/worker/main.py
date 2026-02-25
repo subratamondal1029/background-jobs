0624f82d44fe9e0253d19f0ead01866a5d4fbcad
Subrata Mondal <subratamondal@tutanota.com>
2026-02-25 11:22:12 +0530

    implement message consumption and retry logic in preview generator

--- image-preview/worker/main.py
+++ image-preview/worker/main.py
@@ -1,7 +1,18 @@
 from modules.getenv import getenv
+from modules.rabbitmq import connect_to_rabbitmq
+from modules.preview_generator import generate_preview
+from modules.retry_queue_producer import init as retry_init
+
+IMAGE_PREVIEW_QUEUE = getenv("QUEUE_NAME")
 
 def main():
-    print("Hello from worker!")
+    ch = connect_to_rabbitmq()
+    retry_init(ch)
+
+    ch.basic_consume(queue=IMAGE_PREVIEW_QUEUE, on_message_callback=generate_preview)
+
+    print("Worker Started")
+    ch.start_consuming()
 
 
 if __name__ == "__main__":
--- image-preview/worker/modules/preview_generator.py
+++ image-preview/worker/modules/preview_generator.py
@@ -1,9 +1,46 @@
 import json
+from decimal import Decimal
 from typing import TypedDict
+
+from pika import spec
+from pika.adapters.blocking_connection import BlockingChannel
+
+from getenv import getenv
+from retry_queue_producer import publish_retry
+
+MAX_RETRIES = int(getenv("MAX_RETRIES"))
+
 
 class Data(TypedDict):
     size: int
+    imageId: str
+    jobId: str
 
 
-def generate_preview(data):
-    pass 
+def generate_preview(
+    channel: BlockingChannel,
+    method: spec.Basic.Deliver,
+    properties: spec.BasicProperties,
+    body: bytes,
+):
+    try:
+        headers = properties.headers or {}        
+        data: Data = json.loads(body)
+        print(data)
+
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as e:
+        raw_retry_count = headers.get("x-retry-count", 0)
+        # get in proper int type
+        if isinstance(raw_retry_count, (int, Decimal, str, bytes, bytearray)):
+            retry_count: int = int(raw_retry_count)
+        else:
+            retry_count: int = 0
+
+        if retry_count >= MAX_RETRIES:
+            # add to update database
+            print(f"Max retries exceeded for job {data['jobId']}")
+            channel.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+            return
+        
+        publish_retry(channel, body, retry_count + 1)
